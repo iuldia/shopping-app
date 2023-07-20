fa3b02d740a355c3a3e524b2d7eed3ff5d165861
iuldia <iuldia.pop@gmail.com>
2023-07-20 18:43:44 +0600

    production deployment configuration

--- shopping-lists/db/database.js
+++ shopping-lists/db/database.js
@@ -1,12 +1,7 @@
 import { Client } from '../deps.js'
 
-const dbClient = new Client({
-  user: "username",
-  database: "database",
-  hostname: "database-p1-5211bc91-0b55-4f09-b2ef-4526738ec721",
-  password: "password",
-  port: 5432
-});
+const dbClient = new Client(Deno.env.get("DATABASE_URL") || "postgres://username:password@database-p1-5211bc91-0b55-4f09-b2ef-4526738ec721:5432/database?sslmode=require");
+
 await dbClient.connect();
 
 export {dbClient}
